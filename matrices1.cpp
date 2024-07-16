bb751189ccaf9487004a479c9e2866c79dbbb1b3
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-15 21:09:52 -0500

    Agrega funcion suma para hallar la sumatoria de elementos encima de la diagonal principal

--- matrices1.cpp
+++ matrices1.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 void leer(int m,int a[][100]);
+void suma(int m,int a[][100]);
 
 int main(){
     int n;
     cout<<"Orden de matriz cuadrada: ";cin>>n;
     int A[n][100];
     leer(n,A);
+    suma(n,A);
     return 0;
 }
 
@@ -17,4 +19,15 @@ void leer(int m,int a[][100]){
             cout<<"Valor de ["<<i+1<<"]"<<"["<<j+1<<"]: ";cin>>a[i][j];
         }
     }
+}
+void suma(int m,int a[][100]){
+    int s=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            if(i<j){
+                s=s+a[i][j];
+            }
+        }
+    }
+    cout<<"La sumatoria es: "<<s<<endl;
 }
