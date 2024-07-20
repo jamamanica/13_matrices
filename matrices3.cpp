eac9e4b8fe3686df2fd560c9b9b3900e978d5d31
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-19 20:15:48 -0500

    Implementa funcion imprimir

--- matrices3.cpp
+++ matrices3.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 void matriz(int m,int a[][100]);
+void imprimir(int m,int a[][100]);
 
 int main(){
     int n;
     cout<<"Valor de n: ";cin>>n;
     int A[n][100];
     matriz(n,A);
+    imprimir(n,A);
     return 0;
 }
 void matriz(int m,int a[][100]){
@@ -18,6 +20,13 @@ void matriz(int m,int a[][100]){
             }else{
                 a[i][j]=0;
             }
+        }
+    }
+}
+void imprimir(int m,int a[][100]){
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m*3;j++){
+            cout<<"["<<i+1<<"]["<<j+1<<"]: "<<a[i][j]<<endl;
         }
     }
 }
