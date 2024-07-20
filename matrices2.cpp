f16b65feda30bca7ecbc2458ce906cea48174233
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-19 19:22:19 -0500

    Agrega funcion suma

--- matrices2.cpp
+++ matrices2.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 void leer(int m,int a[][100]);
+void suma(int m,int a[][100]);
 
 int main(){
     int n;
     cout<<"Orden de matriz cuadrada: ";cin>>n;
     int A[n][100];
     leer(n,A);
+    cout<<"--------------------"<<endl;
+    suma(n,A);
     return 0;
 }
 
@@ -17,4 +20,15 @@ void leer(int m,int a[][100]){
             cout<<"Valor de ["<<i+1<<"]["<<j+1<<"]: ";cin>>a[i][j];
         }
     }
+}
+void suma(int m,int a[][100]){
+    int s=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            if(i+j==m-1){
+                s=s+a[i][j];
+            }
+        }
+    }
+    cout<<"La suma de la diagonal secundaria es: "<<s<<endl;
 }
