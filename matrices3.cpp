72bff0cad87943c211151007b3eebf3fcf7d6511
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-19 20:14:19 -0500

    Agrega matrices 3: establece la funcion matriz

---
+++ matrices3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void matriz(int m,int a[][100]);
+
+int main(){
+    int n;
+    cout<<"Valor de n: ";cin>>n;
+    int A[n][100];
+    matriz(n,A);
+    return 0;
+}
+void matriz(int m,int a[][100]){
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m*3;j++){
+            if(i==0||j==0||i==m-1||j==m*3-1){
+                a[i][j]=1;
+            }else{
+                a[i][j]=0;
+            }
+        }
+    }
+}
